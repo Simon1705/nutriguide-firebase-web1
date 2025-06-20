e509e1c5b477b2ee3ee0fee9fcca533547499574
Simon <simonpeter@student.telkomuniversity.ac.id>
2025-06-20 21:29:40 +0700

    haduh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 git config --global --add safe.directory '*'
 
 # Install required packages
-apt-get update && apt-get install -y curl git unzip xz-utils zip libglu1-mesa
+yum update -y && yum install -y curl git unzip xz tar
 
 # Download and extract Flutter
 curl -L https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.19.0-stable.tar.xz | tar -xJ
